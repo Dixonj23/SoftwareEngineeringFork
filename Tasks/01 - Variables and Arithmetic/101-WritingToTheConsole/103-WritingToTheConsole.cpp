697ab08501cdbeff1fe6f6b6c5d5668dfd734ed4
Jack Dixon <jack.dixon@students.plymouth.ac.uk>
2023-09-28 10:29:42 +0100

    Task Answers

--- Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
+++ Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
@@ -18,6 +18,8 @@ int main()
     float radius = 10.0f;
     float area = 3.415926 * radius * radius;
 
+    printf("area = %5.3f\n", area);
+
     // TASK(b) - Use printf to display the value of the variable area. Look at the lecture slides and find out how to limit this to 3 decimal places
     // Write solution here
     
