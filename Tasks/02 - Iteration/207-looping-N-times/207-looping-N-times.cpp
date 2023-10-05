2f9c755b6fb8020613a910f772cc9de46fe0f3a5
Jack Dixon <jack.dixon@students.plymouth.ac.uk>
2023-10-05 10:25:11 +0100

    Tasks completed

--- Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
+++ Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
@@ -6,25 +6,51 @@ int main()
     int i;
 
 
-    //For loop (pre-C99 standard)
+    /*For loop(pre - C99 standard)
     for (i = 0; i < 5; i++)
     {
         //Perform code tasks
         printf("i=%d\n", i);
     }
+    */
 
-    // TASK A - count from 10 down to 0 in steps of -1
+    /* TASK A - count from 10 down to 0 in steps of - 1
 
-    // TASK B - count from 10 down to -10 in steps of -2
+    for (i = 10; i != -1; i= i-1)
+    {
 
-    // TASK C - count from 10 to 200 in steps of 10
+        printf("i = %d\n", i);
+    }
+    */
 
-    // TASK D - count from -10 down to 20 in steps of 5
+    /* TASK B - count from 10 down to - 10 in steps of - 2
 
-    // TASK E - can you predict the largest value of i that is displayed?
+    for (i = 10; i >= -10; i = i - 2)
+    {
+        printf("i = %d\n", i);
+    }
+    */
+
+    /* TASK C - count from 10 to 200 in steps of 10
+
+    for (i = 10; i <= 200; i = i + 10)
+    {
+        printf("i = %d\n", i);
+    }
+    */
+
+    /* TASK D - count from - 10 down to 20 in steps of 5
+
+    for (i = -10; i <= 20; i = i + 5)
+    {
+        printf("i = %d\n", i);
+    }
+    */
+
+    // TASK E - can you predict the largest value of i that is displayed ?  Answer = 9
     for (i = 0; i < 12; i += 3)
     {
         //Perform code tasks
         printf("i=%d\n", i);
     }
-}
+} 
--- Tasks/02 - Iteration/211-nested-loops/211-nested-loops.cpp
+++ Tasks/02 - Iteration/211-nested-loops/211-nested-loops.cpp
@@ -1,19 +1,57 @@
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int timetables();
+
 
 int main()
+{
+    int timetables();
+
+    cout << timetables();
+}
+
+
+int task211()
 {
     // Outer loop 
     for (int i = 1; i <= 2; i++)        // Outer loop executes twice
     {
-        printf("Outer loop number %d\n", i);
+        printf("Outer loop %d\n", i);
+
 
         // Inner loop
         for (int j = 1; j <= 5; j++)    // Inner loop executes 5 times for each outer loop run so 10 time in total
         {
-            printf("\tInner loop number %d\n", j);  // note the use of the tab (\t) option at the start to improve the layout
-            
-            //Task - Write another level of nesting here
+            printf("Inner loop %d\n", j);
+            printf("\t\ti,j,k\n");
+            printf("\t\t-----\n");// note the use of the tab (\t) option at the start to improve the layout
 
+            //Task - Write another level of nesting here
+            for (int k = 1; k <= 3; k++)
+            {
+                printf("\t\t%d %d %d\n", i, j, k);
+            }
         }
     }
+    return 0;
+}
+
+int timetables()
+{
+    for (int i = 1; i <= 12; i++)
+    {
+        printf("(%d)\n", i);
+
+
+        for (int j = 1; j <= 12; j++)
+        {
+            int result = i * j;
+            printf("\t%d * %d = %d\n", i, j, result);
+        
+        }
+
+    }
+    return 0;
 }
